36980ec5dc33f7241008b0b47bcf587a06a07e7f
Nayan5001 <arora.nayan999@gmail.com>
2021-01-31 23:10:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res)
+{
+res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res)
+{
+var firstName=req.body.fName;
+var lastName=req.body.lName;
+var email=req.body.email;
+
+var data=
+{
+  members:[
+    {
+      email_address:email,
+      status:"subscribed",
+      merge_fields:
+      {
+        FNAME:firstName,
+        LNAME:lastName
+      }
+    }
+  ]
+};
+
+var jsonData=JSON.stringify(data);
+
+var options={
+url:"https://us7.api.mailchimp.com/3.0/lists/f305fcc871",
+method:"POST",
+headers:
+{
+"Authorization":"Nayan 705c993528db37987113ebf3162521b9-us7"
+},
+body:jsonData
+};
+request(options,function(error,response,body)
+{
+if(error)
+{
+res.sendFile(__dirname+"/failure.html");
+}else{
+  if(response.statusCode===200)
+  {
+    res.sendFile(__dirname+"/success.html");
+  }
+  else{
+    res.sendFile(__dirname+"/failure.html");
+  }
+}
+});
+});
+
+app.post("/failure",function(req,res)
+{
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT||3000,function()
+{
+  console.log("Server running at port 3000");
+});
+
+//API Key
+//705c993528db37987113ebf3162521b9-us7
+//List ID
+//f305fcc871
